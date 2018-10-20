be44fe1ceca5dc8b005ad72242d0017db4892565
Seth Taddiken <sethtaddiken@gmail.com>
2018-10-20 02:03:42 -0600

    Added Bias
    Added bias to the network! Now it can correctly train on data sets with all zeroed-out inputs!

--- main.py
+++ main.py
@@ -16,40 +16,39 @@ X = np.array([[0,0,1],
               [0,0,0],
               [1,1,0]])
 
-for row in range(8):
-    biasX = np.append(X,.....)
-    
+# add bias
+X_b = np.insert(X,[3],[[1],[1],[1],[1],[1],[1],[1],[1]],axis=1)
 
 # output dataset
 Y = np.array([[0,0,1,1,0,1,0,1]]).T
 
-# seed random numbers
+# seed random numbers... I'm told this is "good practice"
 np.random.seed(1)
 
 # initialize weights randomly with mean 0
-syn0 = 2*np.random.random((3,1)) - 1
+syn0 = 2*np.random.random((4,1)) - 1
 
 for i in range(1000000):
     
     # forward propogation
-    l0 = X
+    l0 = X_b
     l1 = nonlin(np.dot(l0,syn0))
     
     # error calculation
     if i%1000 == 0:
         l1_error = ((Y-l1)**2)/2
-        print "Summed Error: ", sum(l1_error), " -- Summed Error Excluding Zero Vector: ", sum(l1_error)-0.125
+        print ("Summed Error: ", sum(l1_error),)
     
     d_l1_error = -(Y-l1)
     
-    #multiply error by the slope of the sigmoid at each value in l1
+    # multiply error by the slope of the sigmoid at each value in l1
     l1_delta = d_l1_error * nonlin(l1,True)
     
     # update weights
     eta = 1
     syn0 -= eta*np.dot(l0.T,l1_delta)
 
-print "\nOutput:"
-print l1
-print "\nSynapse Values:"
-print syn0
+print ("\nOutput:")
+print (l1)
+print ("\nSynapse Values:")
+print (syn0)
