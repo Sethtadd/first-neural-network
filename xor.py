ce4fbfc9eaa12d5fde37c1ecce9489a6c9e55a2f
Seth Taddiken <sethtaddiken@gmail.com>
2018-10-21 02:29:32 -0600

    Made Layer/Weight/Nonlin-Functions More Followable
    Separated layers into layer_in and layer_out so that the error propagation could be dealt with more clearly and understandably

--- xor.py
+++ xor.py
@@ -32,32 +32,34 @@ for i in range(1000000):
     
     # forward propogation
     l0 = X_b
-    l1 = nonlin(np.dot(l0,syn0))
-    l2 = nonlin(np.dot(l1,syn1))
+    l1_in = np.dot(l0,syn0)
+    l1_out = nonlin(l1_in)
+    l2_in = np.dot(l1_out,syn1)
+    l2_out = nonlin(l2_in)
     
     # error calculation
     if i%1000 == 0:
-        l2_error = ((Y-l2)**2)/2
+        l2_error = ((Y-l2_out)**2)/2
         print ("Summed Error: ", sum(l2_error),)
     
-    d_l2_error = -(Y-l2)
+    d_l2_error = -(Y-l2_out)
     
     # multiply error by the slope of the sigmoid at each value in l2
-    l2_delta = d_l2_error * nonlin(l2,deriv=True)
+    l2_delta = d_l2_error * nonlin(l2_out,deriv=True)
     
     l1_error = np.dot(l2_delta,syn1.T) # ATTENTION figure out how this part works
     
-    l1_delta = l1_error * nonlin(l1,deriv=True)
+    l1_delta = l1_error * nonlin(l1_out,deriv=True)
     
     # update weights
     alpha = 10
     syn0 -= alpha*np.dot(l0.T,l1_delta)
-    syn1 -= alpha*np.dot(l1.T,l2_delta)
+    syn1 -= alpha*np.dot(l1_out.T,l2_delta)
 
 print ("\nTotal Error:")
 print (sum(l2_error))
 print ("\nOutput:")
-print (l2)
+print (l2_out)
 print ("\nSynapse 0 Values:")
 print (syn0)
 print ("\nSynapse 1 Values:")
